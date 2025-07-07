0132410decbab668acb0369762378abe76688dae
kiprutokels <kiprutokels@gmail.com>
2025-07-07 13:56:34 +0300

    Fix :Code organization and sql schema

--- CODES/login/connection.php
+++ CODES/login/connection.php
@@ -2,9 +2,8 @@
 $hostName = "localhost";
 $dbUser = "root";
 $dbPassword = "root";
-$dbName = "loginsystem";
+$dbName = "classic_hoteldb";
 $conn = mysqli_connect($hostName, $dbUser, $dbPassword, $dbName);
- if (!$conn){
-    die("something went wrong!");
- }?>
- 
+if (!$conn) {
+   die("something went wrong!");
+}
